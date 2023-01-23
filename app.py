7ecdc1d10d8bc0930a8f5cf05dad2c9e92bbd6dc
Tomasz Chmielewski <123314449+G00d-Habitz@users.noreply.github.com>
2023-01-23 22:23:54 +0100

    Trying to create a low-delay timer
    First time using timeit, not sure if that's the way, but it'll never go over like 0.1 delay at the end i think!
    
    I started with time.sleep(1) and it was pretty bad at losing milliseconds.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-import pygame
+import pygame, timeit
+
+goaltime = int(input("How many seconds would you like to count down from? "))
+starttime = timeit.default_timer()
+
+while timeit.default_timer() - starttime < goaltime:
+    timedown = goaltime - (timeit.default_timer() - starttime)
+    print(f"Current time is: {int((timedown)/60)}:{(int(timedown) % 60)}")
+
+print("Bzzzt! The time is up!")
