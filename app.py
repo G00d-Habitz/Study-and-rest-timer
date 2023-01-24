0e3a0f8e204fb3c7f3c3497b17d0fac62ee77509
G00d-habitz <TomaszGChmielewski@Gmail.com>
2023-01-24 13:09:22 +0100

    Created a display and moved timer to a function

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import pygame, timeit
+from sys import exit
 
-goaltime = int(input("How many seconds would you like to count down from? "))
-starttime = timeit.default_timer()
+def animedoro():
+    goaltime = int(input("How many seconds would you like to count down from? "))
+    starttime = timeit.default_timer()
+    while timeit.default_timer() - starttime < goaltime:
+        timedown = goaltime - (timeit.default_timer() - starttime)
+        print(f"Current time is: {int((timedown)/60)}:{(int(timedown) % 60)}")
+    print("Bzzzt! The time is up!")
 
-while timeit.default_timer() - starttime < goaltime:
-    timedown = goaltime - (timeit.default_timer() - starttime)
-    print(f"Current time is: {int((timedown)/60)}:{(int(timedown) % 60)}")
+pygame.init()
+pygame.display.set_caption("Czas na naukę!")
+screen = pygame.display.set_mode((640, 360))
+clock = pygame.time.Clock()
 
-print("Bzzzt! The time is up!")
+
+while True:
+
+    # user input eventloop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+    screen.fill((94,129,162))
+    
+    pygame.display.update()
+    clock.tick(10)
