cb7082f2efdc25f5a3b2ddb7966686a85e6fa20c
G00d-habitz <TomaszGChmielewski@Gmail.com>
2023-01-26 02:33:02 +0100

    More on the looks of menu, but no functionality

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ def animedoro():
     print("Bzzzt! The time is up!")
 
 pygame.init()
-pygame.display.set_caption("Przerwa dla nauki")
+pygame.display.set_caption("Przerwa i nauka")
 screen = pygame.display.set_mode((640, 360))
 clock = pygame.time.Clock()
 
 # Fonts
 main_font = pygame.font.Font("./fonts/orange juice 2.0.ttf", 69)
 smaller_main_font = pygame.font.Font("./fonts/orange juice 2.0.ttf", 35)
-app_name = main_font.render("Przerwa dla nauki", True, "Black")
+
+# Menu
+app_name = main_font.render("Przerwa i nauka", True, "Black")
 app_name_rect = app_name.get_rect(center = (320, 75))
 menu_start = smaller_main_font.render("-Zacznij naukę", True, "Black" )
 menu_start_rect = menu_start.get_rect(bottomleft = (75, 175))
@@ -25,6 +27,8 @@ menu_ustaw = smaller_main_font.render("-Ustawienia", True, "Black" )
 menu_ustaw_rect = menu_ustaw.get_rect(bottomleft = (75, 225))
 menu_instr = smaller_main_font.render("-Instrukcje", True, "Black" )
 menu_instr_rect = menu_instr.get_rect(bottomleft = (75, 275))
+tonton_studying = pygame.image.load("./images/tonton_studying.gif").convert_alpha()
+user_position_menu = 0
 while True:
 
     # user input eventloop
@@ -39,6 +43,22 @@ while True:
     screen.blit(menu_start,menu_start_rect)
     screen.blit(menu_ustaw,menu_ustaw_rect)
     screen.blit(menu_instr,menu_instr_rect)
-
+    screen.blit(tonton_studying, (300,75))
+    if user_position_menu == 0:
+        menu_start = smaller_main_font.render("-Zacznij naukę", True, "#191970" )
+        menu_ustaw = smaller_main_font.render("-Ustawienia", True, "Black" )
+        menu_instr = smaller_main_font.render("-Instrukcje", True, "Black" )
+    elif user_position_menu == 1:
+        menu_start = smaller_main_font.render("-Zacznij naukę", True, "Black" )
+        menu_ustaw = smaller_main_font.render("-Ustawienia", True, "#191970" )
+        menu_instr = smaller_main_font.render("-Instrukcje", True, "Black" )
+    else:
+        menu_start = smaller_main_font.render("-Zacznij naukę", True, "Black" )
+        menu_ustaw = smaller_main_font.render("-Ustawienia", True, "Black" )
+        menu_instr = smaller_main_font.render("-Instrukcje", True, "#191970" )
+    
+    user_position_menu +=1
+    if user_position_menu > 2:
+        user_position_menu = 0
     pygame.display.update()
-    clock.tick(10)
+    clock.tick(2)
